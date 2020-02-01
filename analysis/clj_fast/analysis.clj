298ecd5340f88b7ac120b6c83797f9730b9aa060
bsless <ben.sless@gmail.com>
2020-02-01 16:49:59 +0200

    Add memoize to analysis and refactor
    Some functions were a bit too specific, so generalize them to fit
    memoize results

--- analysis/clj_fast/analysis.clj
+++ analysis/clj_fast/analysis.clj
@@ -1,4 +1,5 @@
 (ns clj-fast.analysis
+  "Interactive notebook namespace to load, parse and chart benchmark results"
   (:require
    [incanter
     [core :as i]
@@ -9,7 +10,7 @@
   (clojure.edn/read-string (slurp s)))
 
 (def common
-  #{:assoc :merge :get-in :select-keys :assoc-in :update-in})
+  #{:assoc :merge :get-in :select-keys :assoc-in :update-in :memoize})
 
 (defn title
   [bench ctrl n]
@@ -18,77 +19,81 @@
    ["Benchmark" (name bench) "-" (str (name ctrl) ":") n]))
 
 (defn common-charts
-  [raw-data]
-  (reduce
-   (fn [m [run-type run-data]]
-     (let [x :width
-           y :keys
-           by-x (group-by x run-data)
-           by-y (group-by y run-data)
-           y-label "mean execution time (ns)"
-           y-charts
-           (reduce
-            (fn [m [y-val y-data]]
-              (let [y-ds (i/dataset y-data)
-                    x-label "log10 map size (elements)"
-                    chart
-                    (charts/bar-chart
-                     x :mean
-                     :group-by :bench
-                     :data y-ds
-                     :legend true
-                     :title (title run-type y y-val)
-                     :x-label x-label
-                     :y-label y-label)]
-                (assoc m y-val chart)))
-            {}
-            by-y)
-           x-charts
-           (reduce
-            (fn [m [x-val x-data]]
-              (let [x-ds (i/dataset x-data)
-                    x-label "number of elements"
-                    chart
-                    (charts/bar-chart
-                     y :mean
-                     :group-by :bench
-                     :data x-ds
-                     :legend true
-                     :title (title run-type x x-val)
-                     :x-label x-label
-                     :y-label y-label)]
-                (assoc m x-val chart)))
-            {}
-            by-x)]
-       (assoc m run-type {x x-charts y y-charts})))
-   {}
-   (filter (fn [[k _]] (common k)) raw-data)))
+  ([raw-data]
+   (common-charts raw-data :width :keys))
+  ([raw-data x-dim y-dim]
+   (reduce
+    (fn [m [run-type run-data]]
+      (let [by-x (group-by x-dim run-data)
+            by-y (group-by y-dim run-data)
+            y-label "mean execution time (ns)"
+            y-charts
+            (reduce
+             (fn [m [y-val y-data]]
+               (let [y-ds (i/dataset y-data)
+                     x-label (case x-dim
+                               :width "log10 map size (elements)"
+                               :type "data type")
+                     chart
+                     (charts/bar-chart
+                      x-dim :mean
+                      :group-by :bench
+                      :data y-ds
+                      :legend true
+                      :title (title run-type y-dim y-val)
+                      :x-label x-label
+                      :y-label y-label)]
+                 (assoc m y-val chart)))
+             {}
+             by-y)
+            x-charts
+            (reduce
+             (fn [m [x-val x-data]]
+               (let [x-ds (i/dataset x-data)
+                     x-label "number of elements"
+                     chart
+                     (charts/bar-chart
+                      y-dim :mean
+                      :group-by :bench
+                      :data x-ds
+                      :legend true
+                      :title (title run-type x-dim x-val)
+                      :x-label x-label
+                      :y-label y-label)]
+                 (assoc m x-val chart)))
+             {}
+             by-x)]
+        (assoc m run-type {x-dim x-charts y-dim y-charts})))
+    {}
+    (filter (fn [[k _]] (common k)) raw-data))))
 
 (defn chart-get
   [k raw-data]
-  {k
-   {:keys
-    {1
-     (charts/bar-chart
-      :width :mean
-      :group-by :method
-      :data (i/dataset (get raw-data k))
-      :legend true
-      :x-label "width"
-      :y-label "mean (ns)")}}})
+  (when-let [data (get raw-data k)]
+    {k
+     {:keys
+      {1
+       (charts/bar-chart
+        :width :mean
+        :group-by :method
+        :data (i/dataset data)
+        :legend true
+        :x-label "width"
+        :y-label "mean (ns)")}}}))
 
 (defn chart-assoc-rec
   [raw-data]
-  {:assoc-rec
-   {:width
-    {0
-     (charts/bar-chart
-      :keys :mean
-      :group-by :bench
-      :data (i/dataset (get raw-data :assoc-rec))
-      :legend true
-      :x-label "#keys"
-      :y-label "mean (ns)")}}})
+  (when-let [data (get raw-data :assoc-rec)]
+    {:assoc-rec
+     {:width
+      {0
+       (charts/bar-chart
+        :keys :mean
+        :group-by :bench
+        :data (i/dataset data)
+        :legend true
+        :x-label "#keys"
+        :y-label "mean (ns)")}}}))
 
 (defn flatten-map
   [m]
@@ -100,10 +105,18 @@
            {acc m}))]
     (reduce conj (flatten (iter m [])))))
 
+(defn- string
+  [x]
+  (if (keyword? x) (name x) (str x)))
+
 (defn ks->path
   [[run dim num]]
   (let [other-dim (if (= dim :keys) :width :keys)]
-    (clojure.string/join "_" (conj (mapv name [run dim other-dim]) num))))
+    (clojure.string/join
+     "_"
+     (mapv
+      (comp #(clojure.string/replace % #"\?" "-p") string)
+      [run dim other-dim num]))))
 
 (defn set-log-axis!
   [chart e]
@@ -146,20 +159,23 @@
      (chart-assoc-rec raw-data)))
 
   (logify :merge all-charts)
+
+  ;;; Format merge nicely because the results vary widely
   (map (fn [e c] (set-log-axis! c e))
        [3 4 5 6 7]
        (vals (get-in all-charts [:merge :width])))
 
   (write-charts all-charts)
 
-  (def us "µs")
-  (def ns "ns")
+  ;;; memoize results
+  (def raw-data
+    (-> "./benchmarks/memo-clj-fast-bench.edn"
+        load-results))
 
-  (concat
-   (for [[k vs] raw-data
-         m vs
-         :let [bench (:method m (:bench m))]]
-     [k bench (:width m) (:keys m) (:mean m)]))
-  (count *1)
+  (def charts
+    (common-charts
+     raw-data :type :keys))
+
+  (write-charts charts)
 
   )
