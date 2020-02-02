40562db31675ef2440f03f009ebb915d5bed481c
bsless <ben.sless@gmail.com>
2020-02-02 19:16:53 +0200

    Refactor inline macros to use lens

--- src/clj_fast/inline.clj
+++ src/clj_fast/inline.clj
@@ -4,6 +4,7 @@
    [clojure.core :as c]
    [clj-fast.util :as u]
    [clj-fast.core :as f]
+   [clj-fast.lens :as lens]
    [clj-fast.collections.concurrent-hash-map :as chm]))
 
 (defmacro fast-assoc*
@@ -48,14 +49,8 @@
 (defmacro get-some-in
   [m ks]
   {:pre [(u/simple-seq? ks)]}
-  (let [ks (u/simple-seq ks)
-        sym (gensym "m__")
-        steps
-        (map (fn [step] `(if (nil? ~sym) nil (~sym ~step)))
-             ks)]
-    `(let [~sym ~m
-           ~@(interleave (repeat sym) steps)]
-       ~sym)))
+  (lens/get-some (fn [m k] `(~m ~k))
+   m ks))
 
 (defmacro select-keys
   "Like `select-keys` but faster and uses code generation.
@@ -69,61 +64,21 @@
     `(let ~bindings
        ~form)))
 
-(defn- do-assoc-in
-  [m ks v]
-  (let [me {:tag 'clojure.lang.Associative}
-        g (with-meta (gensym "m__") me)
-        gs (repeatedly (count ks) #(with-meta (gensym) me))
-        gs+ (list* g gs)
-        bs
-        (into
-         [g m]
-         (mapcat (fn [g- g k]
-                   [g- `(get ~g ~k)])
-                 (butlast gs)
-                 gs+
-                 ks))
-        iter
-        (fn iter
-          [[sym & syms] [k & ks] v]
-          (if ks
-            `(assoc ~sym ~k ~(iter syms ks v))
-            `(assoc ~sym ~k ~v)))]
-    `(let ~bs
-       ~(iter gs+ ks v))))
-
 (defmacro assoc-in
   [m ks v]
   {:pre [(u/simple-seq? ks)]}
-  (do-assoc-in m (u/simple-seq ks) v))
-
-(defn- do-update-in
-  [m ks f args]
-  (let [me {:tag 'clojure.lang.Associative}
-        g (with-meta (gensym "m__") me)
-        gs (repeatedly (count ks) #(with-meta (gensym) me))
-        gs+ (list* g gs)
-        bs
-        (into
-         [g m]
-         (mapcat (fn [g- g k]
-                   [g- `(get ~g ~k)])
-                 gs
-                 gs+
-                 ks))
-        iter
-        (fn iter
-          [[sym & syms] [k & ks]]
-          (if ks
-            `(assoc ~sym ~k ~(iter syms ks))
-            `(assoc ~sym ~k (~f ~(first syms) ~@args))))]
-    `(let ~bs
-       ~(iter gs+ ks))))
+  (lens/put
+   (fn [m k v] `(c/assoc ~m ~k ~v))
+   (fn [m k] `(c/get ~m ~k))
+   m (u/simple-seq ks) v))
 
 (defmacro update-in
   [m ks f & args]
   {:pre [(u/simple-seq? ks)]}
-  (do-update-in m (u/simple-seq ks) f args))
+  (lens/update
+   (fn [m k v] `(c/assoc ~m ~k ~v))
+   (fn [m k] `(c/get ~m ~k))
+   m (u/simple-seq ks) f args))
 
 (defmacro find-some-in
   [m ks]
