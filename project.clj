b8938239dc23667475322a3e5b22299f9214c48d
bsless <ben.sless@gmail.com>
2020-02-03 10:17:26 +0200

    Clean up direct linking and deps in project.clj

--- project.clj
+++ project.clj
@@ -4,19 +4,17 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :direct-linking true
   :target-path "target/%s"
   :profiles
   {:uberjar
-   [:bench :cli
+   [:direct :bench :cli
     {:main clj-fast.bench
      :aot :all}]
    :dev
    [:bench :prof :cli
     {:source-paths ["analysis"]
      :dependencies
-     [[incanter "1.9.3"]
-      [org.clojure/tools.cli "0.4.2"]]}]
+     [[incanter "1.9.3"]]}]
    :cli {:dependencies [[org.clojure/tools.cli "0.4.2"]]}
    :bench {:source-paths ["bench"]
            :dependencies [[criterium "0.4.5"]
@@ -29,5 +27,6 @@
    :small-heap {:jvm-opts ["-Xmx2g" "-Xms2G"]}
    :g1 {:jvm-opts ["-XX:+UseG1GC"]}
    :parallel {:jvm-opts ["-XX:+UseParallelGC"]}
+   :direct {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
    }
   :repl-options {:init-ns clj-fast.core})
